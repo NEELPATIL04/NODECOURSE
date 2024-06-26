1a0079b46fcc66de31ab88851f3980db65f22865
NEELPATIL04 <neel1234patil1@gmail.com>
2024-06-26 23:29:01 +0530

    all node pratice files updated personmodules

--- personRoutes.js
+++ personRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Person = require('./Person');
+const Person = require('./person');
 router.post('/', async (req, res) => {
     try {
         const data = req.body;
